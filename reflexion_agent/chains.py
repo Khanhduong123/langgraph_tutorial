4ec7eeecf5c74775292a5de979572bbedae79576
Khanhduong123 <khanhbaoduong789@gmail.com>
2025-05-26 15:53:50 +0700

    KhanhDB: add reflexion agent

---
+++ reflexion_agent/chains.py
@@ -0,0 +1,60 @@
+import os
+import datetime
+from dotenv import load_dotenv
+
+load_dotenv()
+from langchain_core.output_parsers.openai_tools import (
+    JsonOutputToolsParser,
+    PydanticToolsParser,
+)
+
+from langchain_core.messages import HumanMessage
+from langchain_core.prompts import ChatPromptTemplate, MessagesPlaceholder
+from langchain_openai import ChatOpenAI
+from schemas import AnswerQuestion
+
+
+llm = ChatOpenAI(model="gpt-4-turbo-preview")
+parser = JsonOutputToolsParser(return_id=True)
+parser_pydantic = PydanticToolsParser(tools=[AnswerQuestion])
+# search query agent using with tools search
+actor_prompt_template = ChatPromptTemplate.from_messages(
+    [
+        (
+            "system",
+            """"You are expert researcher.
+            Current time: {time}
+            1.{first_instruction}
+            2. Reflect and critique your answers. Be sever to maximize improvement.
+            3. Recommend search queries to research information and improve your answers.""",
+        ),
+        MessagesPlaceholder(variable_name="messages"),
+        ("system", "Answer the user's question above using the required format."),
+    ]
+).partial(time=lambda: datetime.datetime.now().isoformat())
+
+
+# The first responder prompt template
+first_responder_prompt_template = actor_prompt_template.partial(
+    first_instruction="Provide a deteiled ~250 word answer.",
+)
+
+# first responder chain
+first_responder = first_responder_prompt_template | llm.bind_tools(
+    tools=[AnswerQuestion], tool_choice="AnswerQuestion"
+)
+
+if __name__ == "__main__":
+    human_message = HumanMessage(
+        content="Write about AI-Powered SOC / autonomous soc problem domain,"
+        "list starups that do that and raised captital."
+    )
+
+    chain = (
+        first_responder_prompt_template
+        | llm.bind_tools(tools=[AnswerQuestion], tool_choice="AnswerQuestion")
+        | parser_pydantic
+    )
+
+    res = chain.invoke(input={"messages": [human_message]})
+    print(res)
